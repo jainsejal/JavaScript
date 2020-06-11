6fc3086fa3ff9bb2a764b2e31258fdb3e41c5aa2
jainsejal <sejaljain1194@gmail.com>
2020-06-11 20:44:09 +0200

    Message count with javascript

---
+++ basic.js
@@ -0,0 +1,5 @@
+var totalWords = "140";
+var message = prompt("type here");
+var messageCount = message.length;
+
+alert("you have written "+ messageCount+" character, you have " +(totalWords-messageCount)+ " left" );
