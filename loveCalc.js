805b5d2f6858c3c4dee92f50ad74e3bcb4af44f0
jainsejal <sejaljain1194@gmail.com>
2020-06-14 15:26:39 +0200

    Calculating love percentage using random()

---
+++ loveCalc.js
@@ -0,0 +1,9 @@
+prompt("Your Name:");
+prompt("Your Partner's Name:");
+var ranNumber = Math.random();
+ranNumber = Math.floor((ranNumber * 100) + 1);
+if(ranNumber > 70){
+  alert("Your Love Percentage is: " + ranNumber +"%. Stay away from candles - your relationship is hot enough already! " );
+  } else  {
+  alert("Your Love Percentage is: " + ranNumber +"%.");
+  }
