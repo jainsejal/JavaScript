72801ee7458ce88f749a641cc1e48742c2576aaa
jainsejal <sejaljain1194@gmail.com>
2020-06-14 16:40:16 +0200

    BMI_Calculator.js

--- BMI_calc.js
+++ BMI_calc.js
@@ -1,5 +1,15 @@
-function bmiCalculator(weight, height){
-  return Math.round(weight/(height*height));
+function bmiCalculator (weight, height){
+    var interpretation = Math.round(weight/(height*height));
+    if(interpretation < 18.5){
+        return "Your BMI is " + interpretation+", so you are underweight.";
+    }
+    if(interpretation >= 18.5 && interpretation <= 24.9){
+        return "Your BMI is " + interpretation+", so you have a normal weight.";
+    }
+    else{
+        return "Your BMI is " + interpretation+", so you are overweight.";
+    }
+    return interpretation;
 }
 var bmi = bmiCalculator(65, 1.8);
 console.log(bmi)
