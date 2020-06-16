578c9880fa75e49e61a98b57389cb4ed92bba1f4
jainsejal <sejaljain1194@gmail.com>
2020-06-16 15:09:05 +0200

    Check for the name

---
+++ guestList.js
@@ -0,0 +1,7 @@
+var guestList = ["Sejal", "Harsh", "Deb", "Rihab", "Shilpa"];
+var name = prompt("Your name:");
+if(guestList.includes(name)=== true){
+    alert("Welcome to the party");
+}else{
+    alert("Better Luck next time");
+}
